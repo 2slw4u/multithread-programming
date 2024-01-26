d23d2c9d2356e076e4748bf7efb625716236e49b
2slw4u <mr.fyed@mail.ru>
2024-01-26 19:19:25 +0700

    Updated the main file The main file (multithread.cpp) used to be of a previous version. Now it's up to date.

--- multithread/multithread.cpp
+++ multithread/multithread.cpp
@@ -9,22 +9,7 @@
 
 using namespace std;
 
-volatile sig_atomic_t ctrl_v_pressed = 0;
-
-struct request {
-	int priority;
-	int groupNumber;
-	request(int priorityInput, int groupNumberInput) {
-		priority = priorityInput;
-		groupNumber = groupNumberInput;
-	}
-};
-
-struct requestComparator {
-	bool operator()(const request& firstRequest, const request& secondRequest) {
-		return (firstRequest.priority < secondRequest.priority);
-	}
-};
+volatile sig_atomic_t ctrl_c_pressed = 0;
 
 int readVariable(string variableType) {
 	string variableName = (variableType == "devices") ? "приборов" : "групп";
@@ -43,22 +28,22 @@ int readVariable(string variableType) {
 	return result;
 }
 
-void requestsProcessor(priority_queue<request, vector<request>, requestComparator>& requests, mutex& currentMutex, int groupNumber, int deviceNumber) {
+void requestsProcessor(priority_queue<int, vector<int>>& requests, mutex& globalMutex, int groupNumber, int deviceNumber) {
 	do {
-		if (requests.empty() && ctrl_v_pressed) {
+		if (requests.empty() && ctrl_c_pressed) {
 			break;
 		}
 		this_thread::sleep_for(chrono::milliseconds(200));
 		//Строка выше необходима для корректного вывода (иначе выводы потоков накладываются друг на друга)
-		currentMutex.lock();
-		if (requests.empty() || !(requests.top().groupNumber == groupNumber)) {
-			currentMutex.unlock();
+		globalMutex.lock();
+		if (requests.empty()) {
+			globalMutex.unlock();
 		}
 		else {
 			requests.pop();
 			double sleepTime = rand() % 10000;
 			cout << "Прибор номер " + to_string(deviceNumber) + " группы номер " + to_string(groupNumber) + " достал и обработал заявку, теперь можно и поспать (" + to_string(sleepTime / 1000) + " секунд).\n";
-			currentMutex.unlock();
+			globalMutex.unlock();
 			this_thread::sleep_for(chrono::milliseconds(int(sleepTime)));
 			cout << "Прибор номер " + to_string(deviceNumber) + " группы номер " + to_string(groupNumber) + " проснулся!\n";
 		}
@@ -67,23 +52,25 @@ void requestsProcessor(priority_queue<request, vector<request>, requestComparato
 	cout << "Прибор номер " + to_string(deviceNumber) + " группы номер " + to_string(groupNumber) + " завершил свою работу навсегда.\n";
 }
 
-void requestsGenerator(priority_queue<request, vector<request>, requestComparator>& requests, int groupsAmount, int storageCapacity) {
-	while (!ctrl_v_pressed) {
+void requestsGenerator(priority_queue<int, vector<int>>& requests, mutex& globalMutex, int groupsAmount, int storageCapacity) {
+	while (!ctrl_c_pressed) {
 		this_thread::sleep_for(chrono::milliseconds(rand() % 1000));
+		globalMutex.lock();
 		if (requests.size() < storageCapacity) {
-			requests.push(request(rand() % 2, rand() % groupsAmount));
+			requests.push(rand() % 2);
 			cout << "Добавлена заявка!\nОбщее число заявок: " + to_string(requests.size()) << endl;
 		}
 		else {
 			cout << "Места больше нет, ждём.\n";
 		}
+		globalMutex.unlock();
 	}
 	cout << "Вы завершили генерацию заявок.\n";
 }
 
 void signal_handler(int signal_number) {
 	if (signal_number == SIGINT) {
-		ctrl_v_pressed = 1;
+		ctrl_c_pressed = 1;
 	}
 }
 
@@ -93,16 +80,16 @@ int main()
 	srand(time(0));
 	setlocale(LC_ALL, "Russian");
 	int storageCapacity, deviceAmount, groupsAmount;
-	priority_queue<request, vector<request>, requestComparator> requests;
+	priority_queue<int, vector<int>> requests;
 	vector<thread> allThreads;
-	mutex Mutex;
+	mutex globalMutex;
 	storageCapacity = readVariable("capacity");
 	deviceAmount = readVariable("devices");
 	groupsAmount = readVariable("groups");
-	thread generatorThread(requestsGenerator, ref(requests), groupsAmount, storageCapacity);
+	thread generatorThread(requestsGenerator, ref(requests), ref(globalMutex), groupsAmount, storageCapacity);
 	for (int i = 0; i < groupsAmount; ++i) {
 		for (int j = 0; j < deviceAmount; ++j) {
-			allThreads.push_back(thread(requestsProcessor, ref(requests), ref(Mutex), i, j));
+			allThreads.push_back(thread(requestsProcessor, ref(requests), ref(globalMutex), i, j));
 		}
 	}
 	for (int i = 0; i < groupsAmount; ++i) {
